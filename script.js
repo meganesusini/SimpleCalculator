d0847f77d72a6215c2fe13e77250769d9dda783e
megane <susini.megane04@gmail.com>
2023-01-30 18:51:47 +0100

    display numbers

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+// Allows to only display the numbers on the calculator
+
+let nbCollection = document.getElementsByClassName("btn btn-light waves-effect");
+let input = document.getElementById("input");
+
+for (let i=0; i<= nbCollection.length; i++) {
+    nbCollection[i].onclick = function() {
+        input.value = nbCollection[i].value;
+    }
+}
