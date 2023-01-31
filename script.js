a41a8e51cf0ecca142f0ffbc1c65b42b47d7f34d
megane <susini.megane04@gmail.com>
2023-01-31 12:34:57 +0100

    display numbers and operators

--- script.js
+++ script.js
@@ -3,16 +3,16 @@
 let nbCollection = document.getElementsByClassName("btn btn-light waves-effect");
 let input = document.getElementById("input");
 
-// for (let i=0; i<= nbCollection.length; i++) {
+// for (let i=0; i< nbCollection.length; i++) {
 //     nbCollection[i].onclick = function() {
 //         input.value = nbCollection[i].value;
 //     }
 // }
 
-// let opeCollection = document.getElementsByClassName("operator btn btn-info");
+let opeCollection = document.getElementsByClassName("operator btn btn-info");
 
 // When we press the equals button, the result is displayed
-// let equal = document.getElementById("equal");
+let equal = document.getElementById("equal");
 // equal.onclick = function() {
 
 // }
@@ -21,5 +21,26 @@ let input = document.getElementById("input");
 let ac = document.getElementById("ac");
 ac.onclick = function() {
     input.value = 0;
+}
+
+let calculation = "";
+
+// We stock the operators and the numbers in an only collection
+let opeAndNbCollection = Array();
+
+for (let i=0; i<opeCollection.length; i++) {
+    opeAndNbCollection.push(opeCollection[i]);
+}
+
+for (let i=0; i<nbCollection.length; i++) {
+    opeAndNbCollection.push(nbCollection[i]);
+}
+
+// When we press the operators button or the numbers button, their value is added to the calculation
+for (let i=0; i<opeAndNbCollection.length; i++) {
+    opeAndNbCollection[i].onclick = function() {
+        calculation += opeAndNbCollection[i].value;
+        input.value = calculation;
+    }
 }
 
